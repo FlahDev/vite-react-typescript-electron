55791e60be517fdc8cd20135007de780fb9a12a1
flah <flah.dev@gmail.com>
2022-06-13 00:13:25 -0300

    add: init

---
+++ src/config/application.tsx
@@ -0,0 +1,14 @@
+// libs
+import React from "react";
+
+// import "./theme/global.css";
+
+function Application() {
+  return (
+    <div style={{ background: "#777", width: "100%", height: "100%" }}>
+      <h4>hello world</h4>
+    </div>
+  );
+}
+
+export { Application };
